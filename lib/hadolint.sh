79b59b9582f0a032aede4147a8e0d8039417b93f
Johan Bergström <bugs@bergstroem.nu>
2021-04-11 12:52:30 -0400

    feature: support custom hadolint path
    Fixes: https://github.com/jbergstroem/hadolint-gh-action/issues/13

--- lib/hadolint.sh
+++ lib/hadolint.sh
@@ -2,6 +2,9 @@
 
 function output_hadolint_version() {
   local HADOLINT_VERSION=""
+  # I cannot pass path directly here; both tests and invoking `hadolint`
+  # directly would fail.
+  alias hadolint='${HADOLINT_PATH}'
   HADOLINT_VERSION="$(hadolint --version | cut -d " " -f 4)"
   echo "::set-output name=hadolint_version::${HADOLINT_VERSION}"
 }
