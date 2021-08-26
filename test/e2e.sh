3afd208cf059674da6562fb367e1226b139196d2
Johan Bergström <bugs@bergstroem.nu>
2021-08-26 09:08:22 -0400

    feat: hadolint 2.7.0

--- test/e2e.sh
+++ test/e2e.sh
@@ -20,7 +20,7 @@ test_default_path_with_dockerfile() {
 }
 
 test_custom_hadolint_path() {
-  # Since test runners should have jq installed, lets find path and set it
+  # Since test runners should have hadolint installed, lets find path and set it
   assert_status_code 0 "hadolint_path=$(which hadolint) dockerfile=fixtures/default-path/Dockerfile ${HL}"
 }
 
