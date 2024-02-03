2e9ad00ad69c58ad55e9d29dc2881fd1ebb887f8
Johan Bergström <bugs@bergstroem.nu>
2024-02-03 13:50:55 -0300

    fix: stricter version parsing
    We now remove artifacts from the version command, avoiding mismatches
    based on what is provided (eg: `2.9.0`) and what hadolint responds with
    (`2.9.0-no-git`) in certain versions.
    
    Also, add e2e tests to cover these and refactor version checking.
    
    PR: https://github.com/jbergstroem/hadolint-gh-action/issues/142
    Closes: https://github.com/jbergstroem/hadolint-gh-action/issues/142

--- lib/hadolint.sh
+++ lib/hadolint.sh
@@ -5,7 +5,7 @@ HADOLINT_GH_ACTION_VERSION="1.12.0"
 
 function output_hadolint_version() {
   local OUTPUT=""
-  OUTPUT=$(eval "${HADOLINT_PATH}" --version | cut -d " " -f 4)
+  OUTPUT=$(eval "${HADOLINT_PATH}" --version | sed 's/-no-git//' | cut -d " " -f 4)
   echo "hadolint_gh_action_version=${HADOLINT_GH_ACTION_VERSION}"
   echo "hadolint_version=${OUTPUT}"
 }
