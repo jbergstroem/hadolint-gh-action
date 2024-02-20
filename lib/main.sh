d659aba0591e98e222db324a356e4c2e9e6bcb88
Johan Bergström <bugs@bergstroem.nu>
2024-02-20 20:17:53 -0300

    fix: change variable notation

--- lib/main.sh
+++ lib/main.sh
@@ -49,7 +49,7 @@ function run() {
   # to how output formatting works.
   if [[ -n "${OUTPUT_FORMAT}" ]]; then
     OUTPUT=$(eval "${HADOLINT_PATH}" --no-fail --no-color "${CONFIG}" -f "${OUTPUT_FORMAT}" "${DOCKERFILE}")
-    echo "hadolint_output=${OUTPUT//$'\n'/'%0A'} >> \$GITHUB_OUTPUT"
+    echo "hadolint_output=${OUTPUT//$'\n'/'%0A'} >> ${GITHUB_OUTPUT}"
   fi
 
   # Eval to remove empty vars
