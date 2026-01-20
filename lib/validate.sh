a5267d172d089841f83b04f7c31f3adf51cf2e16
Johan Bergström <bugs@bergstroem.nu>
2026-01-20 12:46:17 +0000

    chore: refactor

--- lib/validate.sh
+++ lib/validate.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 function validate_error_level() {
-  for error_level in -1 0 1 2; do
-    [[ "${error_level}" == "${1}" ]] && return 0
-  done
-  return 1
+  case "${1}" in -1 | 0 | 1 | 2) return 0 ;; *) return 1 ;; esac
 }
 
 function validate_annotate() {
-  [[ "${1}" == "true" || "${1}" == "false" ]] && return 0 || return 1
+  case "${1}" in true | false) return 0 ;; *) return 1 ;; esac
 }
 
 function validate_output_format() {
-  local -a output_formats=(tty json checkstyle codeclimate gitlab_codeclimate gnu codacy sonarqube sarif)
-  for format in "${output_formats[@]}"; do
-    [[ "${format}" == "${1}" ]] && return 0
-  done
-  return 1
+  case "${1}" in
+  tty | json | checkstyle | codeclimate | gitlab_codeclimate | gnu | codacy | sonarqube | sarif) return 0 ;;
+  *) return 1 ;;
+  esac
 }
