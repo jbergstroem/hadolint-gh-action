6104753b3a0aa1760bfebfd2d97b0f4f1b8541f2
Johan Bergström <bugs@bergstroem.nu>
2026-01-21 10:37:20 +0000

    feat: add advanced_security option for GitHub Advanced Security
    When enabled, hadolint runs with SARIF output format and uploads results
    to GitHub Advanced Security via the codeql-action/upload-sarif action.
    
    PR: https://github.com/jbergstroem/hadolint-gh-action/pull/185
    Closes: https://github.com/jbergstroem/hadolint-gh-action/issues/10
    Refs: https://docs.github.com/en/code-security/how-tos/scan-code-for-vulnerabilities/integrate-with-existing-tools/uploading-a-sarif-file-to-github

--- lib/main.sh
+++ lib/main.sh
@@ -6,6 +6,7 @@ ERRORLEVEL=${error_level:-0}
 ANNOTATE=${annotate:-"true"}
 OUTPUT_FORMAT=${output_format:-}
 HADOLINT_PATH=${hadolint_path:-"hadolint"}
+ADVANCED_SECURITY=${advanced_security:-"false"}
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 CI=${GITHUB_ACTIONS:-"false"}
 # This variable is magic in workflows; it intercepts output and makes it available across jobs
@@ -29,7 +30,8 @@ function run() {
   fi
 
   validate_error_level "${ERRORLEVEL}" || exit_with_error "Provided error level is not supported. Valid values: -1, 0, 1, 2"
-  validate_annotate "${ANNOTATE}" || exit_with_error "Annotate needs to be set to true or false"
+  validate_boolean "${ANNOTATE}" || exit_with_error "Annotate needs to be set to true or false"
+  validate_boolean "${ADVANCED_SECURITY}" || exit_with_error "advanced_security needs to be set to true or false"
   [[ -z "${OUTPUT_FORMAT}" ]] || validate_output_format "${OUTPUT_FORMAT}" ||
     exit_with_error "Invalid format. If set, output format needs to be one of: tty, json, checkstyle, codeclimate, gitlab_codeclimate, gnu, codacy, sonarqube, sarif"
 
@@ -45,6 +47,14 @@ function run() {
     local HADOLINT_OUTPUT_LINE="hadolint_output=\"${OUTPUT//$'\n'/'%0A'}\""
     echo "${HADOLINT_OUTPUT_LINE}"
     [[ "${CI}" == "true" ]] && echo "${HADOLINT_OUTPUT_LINE}" >>"${GITHUB_OUTPUT}"
+  fi
+
+  # If advanced_security is enabled, generate SARIF output to a file
+  if [[ "${ADVANCED_SECURITY}" == "true" ]]; then
+    local SARIF_FILE
+    SARIF_FILE=$(mktemp -t hadolint-sarif.XXXXXX)
+    "${HADOLINT_PATH}" --no-fail --no-color ${CONFIG} -f sarif ${DOCKERFILE} >"${SARIF_FILE}"
+    [[ "${CI}" == "true" ]] && echo "sarif_file=${SARIF_FILE}" >>"${GITHUB_OUTPUT}"
   fi
 
   # Don't care about output if annotate is set to false - exit code is still passed
